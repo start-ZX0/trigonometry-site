c69135bf5cfe9bc3f29fc7fd4dc6f75edb1bcfe0
start-ZX0 <abbzzsszzss@gmail.com>
2026-04-19 22:38:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function goLearn() {
+    window.location.href = "learn.html";
+}
+
+function goTest() {
+    window.location.href = "test.html";
+}
+
+function goHome() {
+    window.location.href = "index.html";
+}
