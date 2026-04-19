8e273ee4ddb2dfb32c99f07c32c8db3fc78f276b
start-ZX0 <abbzzsszzss@gmail.com>
2026-04-20 01:06:55 +0300

    Refactor angle data structure and update event handling

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
 const slider = document.getElementById('angleSlider');
 const vector = document.getElementById('vector');
 const angleDisplay = document.getElementById('angleDisplay');
-const noteBox = document.getElementById('dynamicNote');
 const sinVal = document.getElementById('sinVal');
 const cosVal = document.getElementById('cosVal');
 const tanVal = document.getElementById('tanVal');
 
-const data = [
-    { deg: 0, sin: "0", cos: "1", tan: "0", note: "الزاوية 0: تقع على محور السينات الموجب." },
-    { deg: 30, sin: "1/2", cos: "√3/2", tan: "√3/3", note: "الزاوية 30: تسمى زاوية حادة شهيرة." },
-    { deg: 45, sin: "√2/2", cos: "√2/2", tan: "1", note: "الزاوية 45: يتساوى فيها الجيب وجيب التمام." },
-    { deg: 60, sin: "√3/2", cos: "1/2", tan: "√3", note: "الزاوية 60: متممة للزاوية 30." },
-    { deg: 90, sin: "1", cos: "0", tan: "غير معرف", note: "الزاوية 90: قائمة، تنطبق على محور الصادات." },
-    { deg: 180, sin: "0", cos: "-1", tan: "0", note: "الزاوية 180: زاوية مستقيمة (π راديان)." },
-    { deg: 270, sin: "-1", cos: "0", tan: "غير معرف", note: "الزاوية 270: تنطبق على محور الصادات السالب." },
-    { deg: 360, sin: "0", cos: "1", tan: "0", note: "360 درجة: عودة لنقطة البداية." }
+const angleData = [
+    { deg: 0, s: "0", c: "1", t: "0" },
+    { deg: 30, s: "1/2", c: "√3/2", t: "√3/3" },
+    { deg: 45, s: "√2/2", c: "√2/2", t: "1" },
+    { deg: 60, s: "√3/2", c: "1/2", t: "√3" },
+    { deg: 90, s: "1", c: "0", t: "∞" },
+    { deg: 180, s: "0", c: "-1", t: "0" },
+    { deg: 270, s: "-1", c: "0", t: "∞" },
+    { deg: 360, s: "0", c: "1", t: "0" }
 ];
 
 slider.addEventListener('input', () => {
-    const angle = data[slider.value];
-    angleDisplay.innerText = `الزاوية: ${angle.deg}°`;
-    vector.style.transform = `rotate(${-angle.deg}deg)`;
+    const val = angleData[slider.value];
+    angleDisplay.innerText = `الزاوية المختارة: ${val.deg}°`;
+    vector.style.transform = `rotate(${-val.deg}deg)`;
     
-    sinVal.innerText = angle.sin;
-    cosVal.innerText = angle.cos;
-    tanVal.innerText = angle.tan;
-    
-    noteBox.innerHTML = `<p>${angle.note}</p>`;
+    sinVal.innerText = val.s;
+    cosVal.innerText = val.c;
+    tanVal.innerText = val.t;
 });
